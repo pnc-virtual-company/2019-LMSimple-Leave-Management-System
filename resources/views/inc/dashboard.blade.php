1109361d5e618efd5d6325e55d0deb79f48d9a5d
SovannthaiNEANG <sovannthaineang@gmail.com>
2019-04-26 10:51:43 +0700

    Pull

--- resources/views/inc/dashboard.blade.php
+++ resources/views/inc/dashboard.blade.php
@@ -27,7 +27,7 @@
       <div class="sidebar-wrapper">
         <ul class="nav">
           <li class="nav-item active  ">
-            <a class="nav-link" href="./dashboard.html">
+            <a class="nav-link" href="{{route('home.create')}}">
               <i class="material-icons">dashboard</i>
               <p>Dashboard</p>
             </a>
